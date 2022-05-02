81c88b506d01547d4829aab2140ca76c2078ac80
mbj-45 <92012408+mbj-45@users.noreply.github.com>
2022-05-02 12:50:06 +0200

    name

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,41 @@
 let app = new Vue ({
-  el: '#app',
-  data: {
-    total: 0
-  },
- methods: {
-  key: function(n) {
-    if(this.total == 0) {
-      this.total = n
-    }
-    else {
-      this.total = "" + this.total + n
-    }
-  },
-  clear: function() {
-    this.total = 0
-  },
-  equal: function() {
-    let equal = this.total;
-    return this.total = eval(equal);
-   },
-   pourcentage: function()  {
-    this.total = this.total/100
-   }     
- }
+    el: '#app',
+    data: {
+      total: 0,
+      check: ''
+    },
+   methods: {
 
-})
+    key: function(n) {
+      if(this.total == 0) {
+        this.total = n
+      }
+      
+      else {
+        this.total = "" + this.total + n
+        this.check = "" + this.total
+      }
+    },
+
+    verif: function(o) {
+      if(this.check !== o) {
+          this.total = "" + this.total + o;
+          this.check = o;
+      }
+  },
+
+    clear: function() {
+      this.total = 0
+    },
+
+    equal: function() {
+      let equal = this.total;
+      return this.total = eval(equal);
+     },
+
+     pourcentage: function()  {
+      this.total = this.total/100
+     }     
+   }
+  
+  })
