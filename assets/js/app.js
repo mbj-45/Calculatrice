9fe026190b2c830ea7f0a312026a54adfb884376
YanisLandrieu <91880045+YanisLandrieu@users.noreply.github.com>
2022-05-02 11:44:12 +0200

    js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,20 @@
 let app = new Vue ({
-  el: '#app'
-  
+  el: '#app',
+  data: {
+    total: 0
+  },
+ methods: {
+  key: function(n) {
+    if(this.total == 0) {
+      this.total = n
+    }
+    else {
+      this.total = "" + this.total + n
+    }
+  },
+  clear: function() {
+    this.total = 0
+  }
+ }
+
 })
